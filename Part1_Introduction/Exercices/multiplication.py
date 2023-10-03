09a0a4c2054a184aeae4c71926b4d52b0ddda189
Antoine07 <antoine.lucsko@gmail.com>
2023-10-03 10:57:58 +0200

    multiplication

---
+++ Part1_Introduction/Exercices/multiplication.py
@@ -0,0 +1,6 @@
+"""
+
+1. Créez une boucle pour afficher la table de multiplication de 7 par exemple
+2. Créez une fonction qui permet de calculer l'ensemble de toutes les tables de multiplication
+
+""" 
